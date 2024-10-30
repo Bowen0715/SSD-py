f558a0d4a70519402bfe9c0691e42ad6d1e435f9
Bowen Jin <853405869@qq.com>
2024-10-30 10:52:44 +0800

    ADD SSD

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    author="Bowen Jin",
+    author_email="bowenjin@stu.njmu.edu.cn",
+    version="0.0.1",
+    name="SSD",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "torch",
+        "spams",
+    ],
+    python_requires=">=3",
+    description="Python interface for Supervised Sparse Decomposition (SSD)",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
